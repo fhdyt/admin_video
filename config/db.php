606353445f157d4f0905233d4adad679b93de1c1
fhdyt <fhidayat2206@gmail.com>
2019-06-11 13:42:17 +0700

    db

--- config/db.php
+++ config/db.php
@@ -1,13 +1,15 @@
 <?php
-/**
- * using mysqli_connect for database connection
- */
+// $databaseHost = 'localhost';
+// $databaseName = 'APP_BEAUTY';
+// $databaseUsername = 'fikri';
+// $databasePassword = 'fikri';
+// $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+?>
 
+<?php
 $databaseHost = 'localhost';
-$databaseName = 'APP_BEAUTY';
-$databaseUsername = 'fikri';
-$databasePassword = 'fikri';
-
+$databaseName = 'u218634996_ab';
+$databaseUsername = 'u218634996_user2';
+$databasePassword = 'fikrihosting';
 $mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
-
 ?>
