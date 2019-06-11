40fa7ad9ba2b56da0618710a74a89ac204f7d55a
fhdyt <fhidayat2206@gmail.com>
2019-06-11 14:18:32 +0700

    Order by

--- modules/post_list.php
+++ modules/post_list.php
@@ -1,8 +1,7 @@
 <?php
 require_once("../config/db.php");
 error_reporting(0);
-$SISWA_ID    = $_POST['SISWA_ID'];
-$query  = "SELECT * FROM VIDEO";
+$query  = "SELECT * FROM VIDEO ORDER BY ID DESC";
 $result = mysqli_query($mysqli,$query)or die(mysqli_error());
 $num_row = mysqli_num_rows($result);
 if(empty($num_row)){
@@ -15,13 +14,13 @@ $no++;
   $tanggal = strtotime($row['TANGGAL']);
   $id = date(('m-d'), $tanggal);
 	echo "<tr class='".$class."'>";
-	echo "<td>".$no."</td>";
+	echo "<td></td>";
     echo "<td>".$row['ID_VIDEO']."</td>";
     echo "<td>".$row['JUDUL']."</td>";
     echo "<td>".$row['KATEGORI']."</td>";
     echo "<td>".$row['IMAGE']."</td>";
     echo "<td>".$row['VIDEO']."</td>";
-    echo "<td><a class='btn btn-default detail_post' onclick='post_detail(".$row['ID'].")'>Detail</a> </td>";
+  //  echo "<td><a class='btn btn-default detail_post' onclick='post_detail(".$row['ID'].")'>Detail</a> </td>";
     echo "</tr>";
 }
   }
